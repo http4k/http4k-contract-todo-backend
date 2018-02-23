e6b5d1673ca4b3cdd6cf6d681010c66c19f9a578
David Denton <denton.david@gmail.com>
2018-02-23 17:48:48 +0000

    upgrade http4k

--- src/main/kotlin/org/http4k/todo/TodoBackendApp.kt
+++ src/main/kotlin/org/http4k/todo/TodoBackendApp.kt
@@ -18,7 +18,6 @@ import org.http4k.filter.DebuggingFilters
 import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson.auto
 import org.http4k.lens.Path
-import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
@@ -40,17 +39,15 @@ fun main(args: Array<String>) {
     fun save(): HttpHandler = { Response(OK).with(todoBody of todos.save(null, todoBody.extract(it))) }
 
     globalFilters.then(
-        routes(
-            contract(
-                Path.of("id") bindContract GET to ::lookup,
-                Path.of("id") bindContract PATCH to ::patch,
-                Path.of("id") bindContract DELETE to ::delete,
-                "/" bindContract GET to list(),
-                "/" bindContract POST to save(),
-                "/" bindContract DELETE to clear()
-            )
-        ))
-        .asServer(Jetty(port.toInt())).start().block()
+        contract(
+            Path.of("id") bindContract GET to ::lookup,
+            Path.of("id") bindContract PATCH to ::patch,
+            Path.of("id") bindContract DELETE to ::delete,
+            "/" bindContract GET to list(),
+            "/" bindContract POST to save(),
+            "/" bindContract DELETE to clear()
+        )
+    ).asServer(Jetty(port.toInt())).start().block()
 }
 
 data class TodoEntry(val id: String? = null, val url: String? = null, val title: String? = null, val order: Int? = 0, val completed: Boolean? = false)
